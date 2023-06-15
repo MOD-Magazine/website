601b831839319b585dffc9f1f0c53b7b75a75788
ickerio <harry.ickeringill@gmail.com>
2023-06-16 01:23:52 +1000

    not finished contributor page

--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -1,5 +1,19 @@
+<script>
+	import Contributor from './Contributor.svelte';
+
+    export let data;
+</script>
+
 <h1>Contribute</h1>
 
 <p>Help us</p>
 
-<p>List of people and images of people who have helped</p>
+<h3>Writers</h3>
+{#each data.writers as writer}
+    <Contributor {...writer} />
+{/each}
+
+<h3>Developers</h3>
+{#each data.writers as writer}
+    <Contributor {...writer} />
+{/each}
