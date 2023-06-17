1634d6690c0e671ba75132d7c7cd8938938194dd
PugzAreCute <me@pugzarecute.com>
2023-06-17 13:48:12 +0530

    Get latest issue from the issues json

--- src/routes/latest/+page.js
+++ src/routes/latest/+page.js
@@ -1,7 +1,14 @@
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
-export function load() {
-    // TODO: not hardcoded
-    throw redirect(301, '/issues/june-2023')
+export async function load({fetch}) {
+    const issues = JSON.parse(await (await fetch("test/test_issues.json")).text())
+
+    //Get latest non-divider issue
+    let index=0
+    // Untill Divider is null and url is not null increment index
+    while(!(issues.issues[index].divider == null && issues.issues[index].url !=null)){
+        index++
+    }
+    throw redirect(301, issues.issues[index].url)
 }
