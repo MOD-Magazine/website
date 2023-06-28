d70317d0da565725dbe522b5d40a46174b44f39c
James (Jamalam) <james@jamalam.tech>
2023-06-28 07:42:01 +0100

    feat: discord redirect

---
+++ src/routes/discord/+page.server.js
@@ -0,0 +1,6 @@
+import { redirect } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+    throw redirect(301, "https://discord.com/invite/9Dd7JBgVCS");
+}
