c1e0eaf3027d4165850f090140017d14cb0bcd82
James (Jamalam) <james@jamalam.tech>
2023-07-11 11:16:30 +0100

    start design work

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <header>
-	<a href="/">
+	<a id="logo-link" href="/" aria-hidden="true">
 		<span class="logo">{@html Logo}</span>
 	</a>
 	<nav>
-		<a href="/about">About</a>
-		<a href="/issues">Articles</a>
-		<a href="/contact">Contact</a>
+		<a href="/about"><span>About</span></a>
+		<a href="/issues"><span>Articles</span></a>
+		<a href="/contact"><span>Contact</span></a>
 	</nav>
 </header>
 
@@ -19,21 +19,23 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		padding: 1rem;
-		background-color: var(--accent);
+		padding: 0.75rem;
+		background-color: #1A0716;
 	}
 
 	nav {
+		width: 100%;
 		display: flex;
 		flex-direction: row;
-		gap: 1rem;
-		padding-top: 0.5rem;
-		padding-bottom: 0.5rem;
+		justify-content: space-between;
+		gap: 1.5rem;
+		padding-top: 0.25rem;
+		padding-bottom: 0.25rem;
 	}
 
 	.logo {
 		display: inline-block;
-		height: 3rem;
+		height: 3.5rem;
 	}
 
 	:global(.logo svg) {
@@ -41,16 +43,25 @@
 		width: 100%;
 	}
 
-	a:not(.logo) {
+	a:not(#logo-link) {
+		width: 33.3%;
+		text-align: center;
 		text-transform: uppercase;
 		text-decoration: none;
-		color: var(--text);
+		font-size: 1.25rem;
+		font-weight: 700;
+		/* make this a var when the palette is finalized */
+		color: #B37012;
 	}
 
 	@media (min-width: 768px) {
 		header {
 			flex-direction: row;
 			justify-content: space-between;
+		}
+
+		nav {
+			width: auto;
 		}
 	}
 </style>
--- src/routes/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Welcome to MOD! Magazine</h1>
-<p>blah blah blah</p>
